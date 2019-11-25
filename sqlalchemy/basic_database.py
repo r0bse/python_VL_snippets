7900796d2b990b4060f9e4bfa75a1ad9ab9ebe9e
robert <robert.schroeder@dbfuhrpark.com>
2019-11-25 01:05:07 +0100

    added sqlalchemy snippet

---
+++ sqlalchemy/basic_database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker, Session
+from sqlalchemy import create_engine
+
+
+SqlAlchemyBase = declarative_base()
+
+class User(SqlAlchemyBase):
+
+    __tablename__ = "user"
+
+    id = Column(Integer,  primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+
+    def __init__(self,  name: str):
+        self.name = name
+
+    def __str__(self):
+        return "User [id: {0}, name: {1}]".format(self.id, self.name)
+
+if __name__ == "__main__":
+
+    engine = create_engine("sqlite:///data.db")
+    SqlAlchemyBase.metadata.create_all(engine)
+    Session = sessionmaker(engine)
+    session = Session()
