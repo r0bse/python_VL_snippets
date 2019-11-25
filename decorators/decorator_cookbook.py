f4b90de53f1662a157238c70796f1609d99c86fe
robert <robert.schroeder@dbfuhrpark.com>
2019-11-25 01:10:09 +0100

    restructuring

--- decorators/decorator_cookbook.py
+++ decorators/decorator_cookbook.py
@@ -4,10 +4,10 @@ def decorator_name(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         # 1. Code der ausgeführt werden soll bevor die dekorierte Funktion aufgerufen wird
-        if (1 == 1):
+        if ("some property" == "some value"):
             # 2. Aufruf der dekorierten Funktoin mit return des Ergebnisses sofern es benötigt wird
             return func(*args, **kwargs)
         # 3. der Code der ausgeführt werden soll ANSTATT die dekorierte Funktion aufzurufen
-        raise ProcessLookupError()
+        raise Exception("React to the situation")
     return wrapper
 
