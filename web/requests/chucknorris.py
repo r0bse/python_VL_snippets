cab6b2221a5ebf12a34efa865d0794b349b36318
robert <robert.schroeder@dbfuhrpark.com>
2020-01-24 18:22:32 +0100

    corrected comment

--- web/requests/chucknorris.py
+++ web/requests/chucknorris.py
@@ -5,7 +5,7 @@ import requests
 def print_random_joke():
     response = requests.get("http://api.icndb.com/jokes/random")
     response_body = response.json() #parse response into json dictionary
-    print("random chuck norris joke: ", response_body["value"]["joke"] )# access json dictionary field "text"
+    print("random chuck norris joke: ", response_body["value"]["joke"] )# access json dictionary field "value" and "joke"
 
 
 if __name__ == "__main__":
