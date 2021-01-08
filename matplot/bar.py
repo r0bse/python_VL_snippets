fa84364b721ff1039357304682536bc6f6a41897
robert <robert.schroeder@dbfuhrpark.com>
2021-01-08 15:18:16 +0100

    added matplot examples

---
+++ matplot/bar.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+plt.bar(["e", "*", "§", "d", "A", ";", "?", "$", "6", "j"], [12, 23, 28, 34, 36, 56, 78, 89, 90, 99], label="Zeichenverteilung", color="g")
+
+# plt.hist(x,y)
+
+if __name__ == '__main__':
+    plt.show()
